ba9eb1f85bc07ef41d0717fb254397c5bceb13cd
somename <someemail>
2015-10-14 16:25:39 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -9,80 +9,80 @@ import org.unioulu.tol.sqat.bsc.BowlingGame;
 public class TestBowlingScoreCalculator {
 
 	@Test
-	public void test1(){
+	public void frame1(){
 	for (int i = 0; i < 10; i++ ){
 		
 		BowlingGame bg = new BowlingGame ();
 			int result= bg.playBowlingGame("x");
 		assertEquals(10,result);
 	}
-		//fail("Not yet implemented");
+		
 	}
 	@Test
-	public void test2() {
+	public void frame() {
 		for (int i = 0; i < 10; i++ ){
 		BowlingGame bg = new BowlingGame ();
 			int result= bg.playBowlingGame("f");
 		assertEquals(0,result);
-		//fail("Not yet implemented");
+		
 		}
 	}
 	
 	@Test
-	public void test3() {
+	public void frame3() {
 		for (int i = 0; i < 10; i++ ){
 		BowlingGame bg = new BowlingGame ();
 			int result= bg.playBowlingGame("g");
 		assertEquals(0,result);
-		//fail("Not yet implemented");
+		
 		}
 	}
 	
 	
 	@Test
-	public void test4() {
+	public void frame() {
 		for (int i = 0; i < 10; i++ ){
 		BowlingGame bg = new BowlingGame ();
 			int result= bg.playBowlingGame("5");
 		assertEquals(5,result);
-		//fail("Not yet implemented");
+		
 		}
 	}
 	
 	@Test
-	public void test5() {
+	public void frame5() {
 		for (int i = 0; i < 10; i++ ){
 		BowlingGame bg = new BowlingGame ();
 			int result= bg.playBowlingGame("5");
 		assertEquals(5,result);
-		//fail("Not yet implemented");
+		
 		}
 	}
 		
 		@Test
-		public void test6() {
+		public void frame() {
 			for (int i = 0; i < 10; i++ ){
 			BowlingGame bg = new BowlingGame ();
 				int result= bg.playBowlingGame("5");
 			assertEquals(5,result);
-			//fail("Not yet implemented");
+			
 			}
 		}
 	
 		
 		@Test
-		public void test7() {
+		public void frame7() {
 			for (int i = 0; i < 10; i++ ){
 			BowlingGame bg = new BowlingGame ();
 				int result= bg.playBowlingGame("5");
 			assertEquals(5,result);
-			//fail("Not yet implemented");
+		
 			}
 		
 	}
 		
 		@Test
-		public void test8() {
+		public void frame8() {
 			for (int i = 0; i < 10; i++ ){
 			BowlingGame bg = new BowlingGame ();
 				int result= bg.playBowlingGame("5");
@@ -93,7 +93,7 @@ public class TestBowlingScoreCalculator {
 		}
 		
 		@Test
-		public void test9() {
+		public void frame9() {
 			for (int i = 0; i < 10; i++ ){
 			BowlingGame bg = new BowlingGame ();
 				int result= bg.playBowlingGame("5");
@@ -104,7 +104,7 @@ public class TestBowlingScoreCalculator {
 		}
 		
 		@Test
-		public void test10() {
+		public void frame10() {
 			for (int i = 0; i < 10; i++ ){
 			BowlingGame bg = new BowlingGame ();
 				int result= bg.playBowlingGame("5");
